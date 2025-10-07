a1ff5048495026e38b3049859d5ea2dc344c6761
Angel <angelguzman10911@gmail.com>
2025-10-07 14:50:09 -0500

    test: probando

--- redis-server/server.js
+++ redis-server/server.js
@@ -1,32 +1,4 @@
 import express from 'express';
-//import { createClient } from 'redis';
-
-/* const client = createClient({ 
-  url: process.env.REDIS_URL || 'redis://:Niux123@40.233.7.206:6379'  // Default local para pruebas
-  // En K8s, setea REDIS_URL=redis://redis:6370 en tu Deployment de la app
-});
-  console.log('🔧 Config  url:', client.options.url);
-
-client.on('error', (err) => {
-  console.log('🛑 Error de Redis:', err.message || err);
-});
-client.on('connect', () => console.log('✅ ¡Conectado a Redis! 🎉'));
-client.on('ready', () => console.log('🚀 Redis listo.'));
-
-let connected = false;
-async function connectRedis() {
-  try {
-    console.log('🔄 Conectando a Redis...');
-    await client.connect();
-    connected = true;
-    console.log('✅ Conexión OK.');
-  } catch (err) {
-    console.log('🛑 Falló conexión:', err.message || err);
-  }
-}
-
-await connectRedis();
- */
 const app = express();
 app.use(express.json());
 
